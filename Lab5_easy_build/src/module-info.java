708bf8391af991ff4e357f97f26fa8d82e6cae4d
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2020-04-11 14:54:34 +0300

    build version added

---
+++ Lab5_easy_build/src/module-info.java
@@ -0,0 +1,7 @@
+module Lab5 {
+requires javafx.fxml;
+requires javafx.controls;
+    requires java.sql;
+
+    opens sample;
+}
---
+++ Lab5_easy_build/src/sample/Product.java
@@ -0,0 +1,25 @@
+package sample;
+
+public final class Product {
+    private final int id;
+    private final String name;
+    private int cost;
+
+    public Product(final int id, final String title, int cost){
+        this.id = id;
+        this.name = title;
+        this.cost = cost;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getCost(){
+        return cost;
+    }
+}
